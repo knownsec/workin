d3a44ef60d0dbbe4367fe39c785be8e62b37bf4c
RussellLuo <luopeng.he@gmail.com>
2014-07-08 15:49:10 +0800

    update

--- workin/exts/auth/discover.py
+++ workin/exts/auth/discover.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from workin.conf import settings
 from workin.extensions import BaseDiscover
 
 from . import settings as config
-
-# explicitly imported to add tables
-# before Base.metadata.create_all
-from . import models
 
 
 class Discover(BaseDiscover):
 
     def execute(self, application):
-        settings.configure(config)
+        application.settings.configure(config)
